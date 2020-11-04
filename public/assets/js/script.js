0847081873f6d17e616176275a81baa82defe185
Piotr Dudek <piotr@Piotrs-MacBook-Air.local>
2020-11-04 14:59:24 -0500

    update rows, columns, list items and buttons

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,29 +1,29 @@
-$(".create-form").on("submit", function(){
- var newburger = {
-    burger_name:  $("#burger_name").val().trim(),
-    devoured: 0
- }
-  $.ajax({
-    url: "/api/burgers",
-    method: "POST",
-    data: newburger
-  }).then(function() {
-    location.reload();
-  })
-})
+// $(".create-form").on("submit", function(){
+//  var newburger = {
+//     burger_name:  $("#burger_name").val().trim(),
+//     devoured: 0
+//  }
+//   $.ajax({
+//     url: "/api/burgers",
+//     method: "POST",
+//     data: newburger
+//   }).then(function() {
+//     location.reload();
+//   })
+// })
 
-$(".devoured").on("click", function(){
-  var id = $(this).attr("data-id")
-  var newburger = {
+// $(".devoured").on("click", function(){
+//   var id = $(this).attr("data-id")
+//   var newburger = {
   
-     devoured: 1
-  }
-  alert("click!")
-   $.ajax({
-     url: "/api/burgers/" + id,
-     method: "PUT",
-     data: newburger
-   }).then(function() {
-     location.reload();
-   })
- })
+//      devoured: 1
+//   }
+//   alert("Devoured!")
+//    $.ajax({
+//      url: "/api/burgers/" + id,
+//      method: "PUT",
+//      data: newburger
+//    }).then(function() {
+//      location.reload();
+//    })
+//  })
