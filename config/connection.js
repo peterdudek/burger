fe94d5b1f3ce65a800c3b45128288c54d56c9ecc
Piotr Dudek <piotr72@gmail.com>
2020-11-04 18:40:04 -0500

    update for JAWS_DB

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,18 @@
 // Dependencies
 const mysql = require("mysql");
-
+var connection;
 // connection parameters
-const connection = mysql.createConnection({
+if(process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
   password: "1410Wilno",
   database: "burgers_db"
 });
+};
 
 // connection
 connection.connect(function(err){
